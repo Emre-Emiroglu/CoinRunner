71f38236945d2cfe0ff59add214a91b54558d57f
Emre Emiroğlu <emirogluemre08@gmail.com>
2022-11-23 20:47:17 +0300

    Own core system inclueded

---
+++ Assets/Scripts/Controllers/CameraController/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace DevShirme.CameraModule
+{
+    public class CameraController : DevShirmeController
+    {
+        #region Fields
+        #endregion
+
+        #region Core
+        public override void Initialize()
+        {
+        }
+        #endregion
+
+    }
+
+}
