769a17460412206cfdcfc649cc5ea17b2a651592
Emre Emiroğlu <emirogluemre08@gmail.com>
2022-11-24 16:29:43 +0300

    Level system preparation

---
+++ Assets/Scripts/Controllers/LevelController/LevelController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DevShirme.LevelModule
+{
+    public class LevelController : DevShirmeController
+    {
+        #region Core
+        public override void Initialize()
+        {
+        }
+        public override void GameStart()
+        {
+        }
+        public override void Reload()
+        {
+        }
+        public override void GameOver()
+        {
+        }
+        public override void GameFail()
+        {
+        }
+        public override void GameSuccess()
+        {
+        }
+        #endregion
+    }
+}
+
---
+++ Assets/Scripts/Controllers/LevelController/Settings/LevelSettings.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DevShirme.LevelModule
+{
+    [CreateAssetMenu(fileName = "LevelSettings", menuName = "DevShirme/Settings/Level Settings", order = 1)]
+    public class LevelSettings : DevSettings
+    {
+        #region Fields
+
+        #endregion
+
+        #region Getters
+
+        #endregion
+    }
+}
+
